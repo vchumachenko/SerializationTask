3a9a59ae13d83e96a8491e1a300ea7fc08c072dc
vchumachenko <v.chumachenko@yahoo.com>
2023-10-03 21:53:26 +0300

    master Done

--- src/main/java/org/nox/Human.java
+++ src/main/java/org/nox/Human.java
@@ -1,2 +1,42 @@
-package org.nox;public class Human {
+package org.nox;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+public class Human implements Serializable {
+  private String name;
+  private int age;
+  private transient String career;
+
+  public Human(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public void setCareer() {
+    if (age >= 0 && age <= 3) {
+      career = "Сидит дома";
+    } else if (age >= 3 && age <= 7) {
+      career = "Ходит в детский сад";
+    } else if (age >= 7 && age <= 18) {
+      career = "Учится в школе";
+    } else if (age >= 17 && age <= 23) {
+      career = "Учится в институте";
+    } else if (age >= 24 && age <= 65) {
+      career = "Работает";
+    } else if (age >= 65) {
+      career = "На пенсии";
+    }
+  }
+  // При десериализации вызывается этот метод, в котором вычисляется career
+  private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+    in.defaultReadObject();
+    setCareer();
+  }
+
+  @Override
+  public String toString() {
+    return "Имя: " + name + "; Возраст: " + age + "; Род деятельности: " + career;
+  }
 }
--- src/main/java/org/nox/Main.java
+++ src/main/java/org/nox/Main.java
@@ -1,7 +1,33 @@
 package org.nox;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+  public static void main(String[] args) throws IOException, ClassNotFoundException {
+    List<Human> humans = new ArrayList<>();
+    humans.add(new Human("Анна", 3));
+    humans.add(new Human("Татьяна", 6));
+    humans.add(new Human("Богдан", 12));
+    humans.add(new Human("Таисия", 19));
+    humans.add(new Human("Егор", 37));
+    humans.add(new Human("Александр", 65));
+
+    // Сериализую список в файл
+    FileOutputStream fos = new FileOutputStream("humans.bin");
+    ObjectOutputStream oos = new ObjectOutputStream(fos);
+    oos.writeObject(humans);
+    oos.close();
+
+    // Десериализуею обратно в список
+    FileInputStream fis = new FileInputStream("humans.bin");
+    ObjectInputStream ois = new ObjectInputStream(fis);
+    List<Human> dHumans = (List<Human>) ois.readObject();
+    ois.close();
+
+    for (Human h : dHumans) {
+      System.out.println(h);
     }
+  }
 }
